88a38af31eae4e047e5a8452f5782ae66d2fd252
islam zamkan <131667724+eslamzamkan@users.noreply.github.com>
2023-10-06 02:22:42 +0300

    Update lab_solution.sql

--- day10/lab_solution.sql
+++ day10/lab_solution.sql
@@ -83,3 +83,99 @@ declare @std int = 1 , @end int = 10
 			INSERT into #seq values (next value for dbo.seqq)
 				set @std +=1
 		end 
+
+/*2.In Company_SD in employee table Check if Gender='M' add 'Mr Befor Employee name    
+else if Gender='F' add Mrs Befor Employee name  then display all names  
+use cursor for update*/
+declare c1 cursor
+for 
+select gender
+from Employee
+for update
+
+declare @name varchar(10), @gender char
+
+open c1
+
+fetch c1 into @gender
+
+while @@FETCH_STATUS = 0
+begin
+    if @gender ='M'
+    begin 
+		update Employee
+		set Fname = CONCAT('Mr' , ' ' , Fname)
+		where current of c1
+    end
+	else
+	begin 
+		update Employee
+		set Fname = CONCAT('Mrs' , ' ' , Fname)
+		where current of c1
+    end
+fetch c1 into @gender
+end
+
+close c1
+deallocate c1
+
+select * from Employee
+
+/*In ITI database Count times that amr apper after ahmed in student table in st_Fname column (using the cursor)  A*/
+declare c1 cursor 
+for 
+select Student.St_Fname
+from Student
+
+for read only
+
+declare @name varchar(10), @counter int = 0, @container int = 0
+
+open c1
+fetch c1 into @name 
+
+while @@FETCH_STATUS =0
+begin
+if @name = 'ahmed'
+    set @container +=1
+if @name = 'amr' and @container = 1
+		begin 
+			set @counter +=1
+			set @container = 0
+		end
+fetch c1 into @name
+end
+
+select @counter
+
+close c1
+deallocate c1
+
+select * from Student
+
+---------------------------------or-------------------------
+
+declare c1 cursor 
+for 
+select  Student.St_Fname, LEAD(St_Fname)over(order by St_id) as next_name
+from Student
+for read only
+
+declare @stu_name varchar(10), @stu_next varchar(10), @count int = 0
+
+open c1
+fetch c1 into @stu_name , @stu_next
+
+while @@FETCH_STATUS =0
+begin
+if @stu_name ='ahmed' and @stu_next = 'amr'
+	set @count+=1
+
+fetch c1 into @stu_name , @stu_next
+end
+
+select @count
+
+close c1
+deallocate c1
+
